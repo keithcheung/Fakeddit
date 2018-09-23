230e6b6b9e0dc2178bb3de5b469057630b399356
keithcheung <keith.cheung41@gmail.com>
2018-09-23 19:19:23 -0400

    express app init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// app.use('/graphql', graphqlHTTP({
+//   schema, graphiql: true
+// }));
+
+app.listen(port, () => {
+  console.log(`Now listening to ${port}`);
+});
